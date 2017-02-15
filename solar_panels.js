e030f4d4c044520d182ea9337c468bc0cf1e9052
Nmuta Jones <nmuta1@gmail.com>
2017-02-15 10:11:47 -0700

    optimization

--- solar_panels.js
+++ solar_panels.js
@@ -1,7 +1,9 @@
 
-var resistance = 345;
+var resistance = 45;
 var panel1 = 1000, panel2 = 2345;
-
 var output = (panel1+panel2) - resistance;
+console.log("output is "+output);
 
-console.log("output is "+output); 
+
+
+console.log("Houston, we don't have any problems. All clear");
